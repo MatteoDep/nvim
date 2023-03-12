ad9db6bed46974f532c7eff7db6b83a61f9cb7c0
MatteoDep <matteo.dep97@gmail.com>
2023-03-13 00:29:55 +0100

    fixed luasnip mappings

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,11 +1,11 @@
 require("luasnip.loaders.from_vscode").lazy_load()
 
-vim.keymap.set('i', '<silent><expr> <Tab>', [[luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' ]], {silent=true, expr=true})
-vim.keymap.set('i', '<S-Tab>', [[lua require'luasnip'.jump(-1)<Cr>]], {silent=true})
+vim.keymap.set('i', '<C-j>', [[luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<C-j>' ]], {silent=true, expr=true})
+vim.keymap.set('i', '<C-k>', [[lua require'luasnip'.jump(-1)<Cr>]], {silent=true})
 
-vim.keymap.set('s', '<Tab>', [[lua require('luasnip').jump(1)<Cr>]], {silent=true})
-vim.keymap.set('s', '<S-Tab>', [[lua require('luasnip').jump(-1)<Cr>]], {silent=true})
+vim.keymap.set('s', '<C-j>', [[lua require('luasnip').jump(1)<Cr>]], {silent=true})
+vim.keymap.set('s', '<C-k>', [[lua require('luasnip').jump(-1)<Cr>]], {silent=true})
 
 -- For changing choices in choiceNodes (not strictly necessary for a basic setup).
-vim.keymap.set('i', '<silent><expr> <C-E>', [[luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>']], {silent=true, expr=true})
-vim.keymap.set('s', '<silent><expr> <C-E>', [[luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>']], {silent=true, expr=true})
+vim.keymap.set('i', '<C-E>', [[luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>']], {silent=true, expr=true})
+vim.keymap.set('s', '<C-E>', [[luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>']], {silent=true, expr=true})
