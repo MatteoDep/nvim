4c893730a7eab349727fcecdf7c0fe81ace3f045
MatteoDep <matteo.dep97@gmail.com>
2023-03-14 11:33:20 +0100

    git changes maps

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -9,3 +9,7 @@ require('gitsigns').setup {
     changedelete = { text = '~' },
   },
 }
+
+vim.keymap.set('n', '<leader>gg', '<cmd>Gitsigns setqflist<CR>', {desc="[G]it changes qflist"})
+vim.keymap.set('n', '<leader>gn', '<cmd>Gitsigns next_hunk<CR>', {desc="[G]it changes next"})
+vim.keymap.set('n', '<leader>gp', '<cmd>Gitsigns prev_hunk<CR>', {desc="[G]it changes prev"})
