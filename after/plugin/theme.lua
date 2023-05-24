6c12c2990560eda0710141bb03c02517a541f18f
MatteoDep <matteo.dep97@gmail.com>
2023-05-24 12:54:59 +0200

    lualine customizations

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -19,16 +19,43 @@ end
 -- lualine
 require('lualine').setup {
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     theme = settings.lualine_theme,
     component_separators = '|',
     section_separators = '',
   },
-  winbar = {
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {
+      {
+        'branch',
+        draw_empty = true,
+      },
+      'diff',
+      {
+        'diagnostics',
+        icons_enabled = false,
+      },
+    },
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      },
+    },
+    lualine_x = {
+      'encoding',
+      'fileformat',
+      'filetype',
+    },
+    lualine_y = {'progress'},
+    lualine_z = {'location'},
+  },
+  inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {},
   },
@@ -36,13 +63,18 @@ require('lualine').setup {
     lualine_a = {
       {
         'buffers',
-        max_length = vim.o.columns
-      }
+        max_length = vim.o.columns * 9 / 10,
+        symbols = {
+          modified = ' ',      -- Text to show when the buffer is modified
+          alternate_file = '# ', -- Text to show to identify the alternate file
+          directory =  ' ',     -- Text to show when the buffer is a directory
+        },
+      },
     },
     lualine_b = {},
     lualine_c = {},
     lualine_x = {},
     lualine_y = {},
     lualine_z = { 'tabs' },
-  }
+  },
 }
