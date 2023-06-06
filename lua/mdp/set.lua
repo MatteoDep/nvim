7e8df71dc131626c13ae4dec72cb60f1a45fab93
Matteo De Pellegrin <matteo.depellegrin@kwantis.com>
2023-06-06 16:03:40 +0200

    use absolute path

--- lua/mdp/set.lua
+++ lua/mdp/set.lua
@@ -69,12 +69,12 @@ elseif iswsl() then
     let g:clipboard = {
                 \   'name': 'WslClipboard',
                 \   'copy': {
-                \      '+': 'clip.exe',
-                \      '*': 'clip.exe',
+                \      '+': '/mnt/c/Windows/System32/clip.exe',
+                \      '*': '/mnt/c/Windows/System32/clip.exe',
                 \    },
                 \   'paste': {
-                \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-                \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+                \      '+': '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+                \      '*': '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
                 \   },
                 \   'cache_enabled': 0,
                 \ }
