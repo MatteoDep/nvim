a48b97c28bdfeb18550f85c245a4b0b0b34ca403
Matteo De Pellegrin <matteo.depellegrin@kwantis.com>
2024-06-06 12:05:45 +0200

    use catpuccin on lualine

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -8,7 +8,7 @@ vim.cmd.colorscheme("catppuccin")
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = "auto",
+    theme = "catppuccin",
     component_separators = '|',
     section_separators = '',
   },
