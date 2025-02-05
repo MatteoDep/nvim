0560c8671e226bd3c8fa69896cd5e62f03a849d0
MatteoDep <matteo.dep97@gmail.com>
2025-02-05 11:28:22 +0100

    remove nightly

--- lua/mdp/packer.lua
+++ lua/mdp/packer.lua
@@ -82,7 +82,6 @@ require('packer').startup(function(use)
     requires = {
       'nvim-tree/nvim-web-devicons', -- optional, for file icons
     },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
 
   -- commenting with `gc`
